67a896558098698d07a204ec3a6cef2a2d2d2aed
FriedreichBismarck <2360756905@qq.com>
2022-02-24 04:36:46 +0000

    Update at 2022-02-24T04:36:46.865796Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,15 +1,16 @@
 package com.bytelegend;
 
-public class Challenge {
-    public static String hello()
-    {
+public class Chanllenge {
+    public static String hello() {
         return "hello";
     }
-    public static int add(int a,int b)
-    {
-        return a+b;
+
+    public static int add(int a, int b) {
+        return a + b;
     }
+
     public static void main(String[] args) {
         System.out.println(hello());
         System.out.println(add(40, 2));
+    }
     }
