22af154b82734d4918e5f93fa5cf7132122d50e9
Peanuuutz <1415605913@qq.com>
2022-03-24 03:15:04 +0000

    Update at 2022-03-24T03:15:04.850111Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,7 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+    public static String hello() { return "Hello"; }
 
-    public static add() {}
+    public static int add(int a, int b) { return a + b; }
 }
