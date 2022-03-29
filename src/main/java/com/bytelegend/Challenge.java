97e0afb85b243cbf0b7005c1f861a57736c77235
Laura-bf <laurabf6@gmail.com>
2022-03-29 16:19:39 +0000

    Update at 2022-03-29T16:19:39.720863Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,7 +11,7 @@ public class Challenge {
         return wordToPrint;
     }
 
-    public static add(int number1, int number2) {
+    public static int add(int number1, int number2) {
         int sum = number1+number2;
         return sum;
     }
