dc6fd40623354562ed629becfe2ce573e503eeb1
kangming06 <kangming06@hotmail.com>
2022-05-07 03:30:14 +0000

    Update at 2022-05-07T03:30:14.971665Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,11 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+    public static String hello() {
+        return "Hel"+"lo";
+    }
 
-    public static add() {}
+    public static String add(int a,int b) {
+        return (String) a + b;
+    }
 }
