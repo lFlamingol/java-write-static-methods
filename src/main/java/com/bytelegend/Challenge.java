3bfe2f5134ce7f4baeb84ae74610b8766c9d35b6
a575981269 <575981269@QQ.com>
2022-05-12 09:07:36 +0000

    Update at 2022-05-12T09:07:36.222379Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,7 +11,7 @@ public class Challenge {
         System.out.println("hello");
     }
 
-    public static int add() {
-        return 42;
+    public static int add(int a, int b) {
+        return a+b;
     }
 }
