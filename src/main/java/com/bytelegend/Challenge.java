d3af13a261b78007bac3e4b6c8821cfac7e87c9f
chinahzh <2987140244@qq.com>
2022-05-27 05:59:48 +0000

    Update at 2022-05-27T05:59:48.507884Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -3,7 +3,7 @@ package com.bytelegend;
 public class Challenge {
     public static void main(String[] args) {
         System.out.println(hello());
-        System.out.println(add(40, 2));
+        System.out.println(add(2, 2));
     }
 
     public static String hello() {
@@ -11,6 +11,7 @@ public class Challenge {
     }
 
     public static int add(int z,int y) {
+        z = z + 40;
         return z + y;
-    }
+    }j
 }
