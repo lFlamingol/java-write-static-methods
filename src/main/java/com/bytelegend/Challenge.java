6fe4645103ab9ce1389353d9ca2a226ac2f792fe
s9x <rich@xtinction.org>
2022-05-31 17:55:38 +0000

    Update at 2022-05-31T17:55:38.456323Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,15 +1,15 @@
 package com.bytelegend;
 
 public class Challenge {
-    private int a = 2;
-    private int b = 40;
+    private static int a = 2;
+    private static int b = 40;
 
     public static void main(String[] args) {
         System.out.println(hello());
         System.out.println(add(a, b));
     }
 
-    public static void hello() {
+    public static String hello() {
         return "Hello";
     }
 
