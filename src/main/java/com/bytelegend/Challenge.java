990038fc488c4b64bd288953ba0c4d959b8381b7
tianjyTGB <tianjiayi200212@163.com>
2022-06-08 02:26:56 +0000

    Update at 2022-06-08T02:26:56.571419Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,16 +1,17 @@
 package com.bytelegend;
 
-public class Challenge {
+class Challenge {
     public static void main(String[] args) {
         System.out.println(hello());
         System.out.println(add(40, 2));
     }
 
     public static String hello() {
-        return "Hello";
-    }
-
+            System.out.println("Hello");
+            return "Hello";
+        }
     public static int add(int z,int y) {
+        System.out.println("z+y");
         return z + y;
     }
 }
