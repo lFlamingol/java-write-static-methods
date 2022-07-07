cf79f5f341a8b6174e2beba77f670d74b746fee5
agirnob <kakcayigitbaran@gmail.com>
2022-07-07 13:53:09 +0000

    Update at 2022-07-07T13:53:09.323097Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,12 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+    public static hello(String str) {
+        return str;
+    }
 
-    public static add() {}
+    public static int add(int x,int x2) {
+
+        return x+x2;
+    }
 }
