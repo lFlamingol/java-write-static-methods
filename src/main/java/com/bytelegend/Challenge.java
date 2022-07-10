ad18a40cbbe549d07267e5c8a8e22dff00b0c70c
Jungmin-Moon <jmmoon96@gmail.com>
2022-07-10 20:14:11 +0000

    Update at 2022-07-10T20:14:11.398639Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,11 +6,11 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static void hello() {
-        System.out.println("Hello");
+    public static String hello() {
+        return "Hello";
     }
 
-    public static void add(int a, int b) {
-        System.out.println(a + b);
+    public static intr add(int a, int b) {
+        return a + b;
     }
 }
