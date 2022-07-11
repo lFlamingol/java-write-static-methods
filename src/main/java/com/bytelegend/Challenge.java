fd7fccdd1ebbd96ccdd91cca8b2c4e259abe4a5f
Birlinha <omenaribeiro13@gmail.com>
2022-07-11 02:58:55 +0000

    Update at 2022-07-11T02:58:55.268055Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,16 +1,15 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static void main(String[] args) {
-        System.out.println(hello());
-        System.out.println(add(40, 2));
-    }
-
     public static hello() {
         System.out.println("Hello");
     }
 
     public static add(a,b) {
         return a + b ;
+    }
+    public static void main(String[] args) {
+        System.out.println(hello());
+        System.out.println(add(40, 2));
     }
 }
