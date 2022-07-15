f2f677b60f8705578889693a11baae6836a71b67
Euerrur <1822289459@qq.com>
2022-07-15 11:54:47 +0000

    Update at 2022-07-15T11:54:47.205376Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,17 +2,17 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println(hello());
-        System.out.println(add(40, 2));
-    }
+            System.out.println(Test.hello());
+            System.out.println(add(40, 2));
+        }
 
-    public static hello() {
-        String a = "Hello";
-        return a;
-    }
+        public static String hello() {
+            String a = "Hello";
+            return a;
+        }
 
-    public static int add(int a , int b) {
-        int sum = a + b;
-        return sum;
-    }
+        public static int add(int a , int b) {
+            int sum = a + b;
+            return sum;
+        }
 }
