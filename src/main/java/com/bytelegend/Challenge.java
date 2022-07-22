045c14407c3668e28d1d48dccddec6c2ccebfd23
Crystalyyds <2031291160@qq.com>
2022-07-22 02:36:21 +0000

    Update at 2022-07-22T02:36:21.689363Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,7 +10,7 @@ public class Challenge {
         return "Hello";
     }
 
-    public static int add() {
-        return 40+2;
+    public static int add(int a,int b) {
+        return a+b;
     }
 }
