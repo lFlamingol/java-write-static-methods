a3d262d5c146d30c9007e68b163457cb76c8ff30
Jonas3012 <jbgood1289@gmail.com>
2022-08-10 09:06:21 +0000

    Update at 2022-08-10T09:06:21.382746Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,16 +1,10 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static void main(String[] args) {
-        System.out.println(hello());
-        System.out.println(add(40, 2));
-    }
 
-    public static void hello(String[] args) {
+    static void hello(String[] args) {
         System.out.println("Hello");
-    }
-
-    public static void addition(String[] args) {
-         System.out.println(add(40, 2));
+    
+        System.out.println("42");
     }
 }
