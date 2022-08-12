e2c43a20206b593b2aa24c4aa04bf9aab4b855fe
JayeHao <15096048532@163.com>
2022-08-12 08:42:13 +0000

    Update at 2022-08-12T08:42:13.279039Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,7 +10,7 @@ public class Challenge {
         System.out.println("hello");
     }
 
-    public static add(int a,int b) {
+    public static add(int a, int b) {
         System.out.println(a);
     }
 }
