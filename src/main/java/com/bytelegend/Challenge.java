69a218d28ebd720c0f306e885984193634c0df97
slsy0 <1485807135@qq.com>
2022-08-28 05:54:16 +0000

    Update at 2022-08-28T05:54:16.474052Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,11 +6,11 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {
-        return "Hello\n";
+    public static String hello() {
+        return "Hello";
     }
 
-    public static add(int a,int b) {
+    public static int add(int a,int b) {
         return a+b;
     }
 }
