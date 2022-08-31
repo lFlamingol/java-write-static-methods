e18d945b1e7c33d9580fadccdcf6457fc2cfbe2d
ov0o0vo <3035566793@qq.com>
2022-08-31 13:15:00 +0000

    Update at 2022-08-31T13:15:00.388485Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,12 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+    public static String hello() {
+        String a = "hello";
+        return a;
+    }
 
-    public static add() {}
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
