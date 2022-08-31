f2a15e046a041c32132939e02564f48aeff0cf7d
GergertHatby <yanghao9225@outlook.com>
2022-08-31 07:43:50 +0000

    Update at 2022-08-31T07:43:50.994841Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,11 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+    public static hello() {
+        return "mo"
+    }
 
-    public static add() {}
+    public static add(int x,int y) {
+        return x+y
+    }
 }
