2146c95d2f2389a07a239f0b8f5f86a892d69dfe
jenfnn <fwyjenny@gmail.com>
2022-09-11 04:06:55 +0000

    Update at 2022-09-11T04:06:55.111285Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,10 +6,13 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
-        char hello = "Hello";
+    public static String hello() {
+        String hello = "Hello";
         return hello;
-    public static add(int a, int b) {}
-        int c = a + b;
-        return c;
+    }
+        
+    public static int add(int a, int b) {
+        return a + b;
+    }
+        
 }
