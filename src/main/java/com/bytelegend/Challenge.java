429d45f7eb0a6c92b8428e155919c179e95a952d
lhhhhhhhhhhhhhh <lihan881212@gmail.com>
2022-09-21 16:15:37 +0000

    Update at 2022-09-21T16:15:37.845467Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,12 +1,12 @@
 package com.bytelegend;
 
-public class Challenge {
+public class Challenge{
     public static void main(String[] args) {
         System.out.println(hello());
         System.out.println(add(40, 2));
     }
 
-    public static String hello() {return "Hello"}
+    public static String hello() {return "Hello";}
 
     public static int add(int a,int b) {return a+b;}
 }
