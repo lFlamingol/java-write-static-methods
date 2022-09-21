5453127a064bfd951806a2e330192979adb776ab
lhhhhhhhhhhhhhh <lihan881212@gmail.com>
2022-09-21 16:14:58 +0000

    Update at 2022-09-21T16:14:58.302315Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,7 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {return "Hello"}
+    public static String hello() {return "Hello"}
 
-    public static add(int a,int b) {return a+b;}
+    public static int add(int a,int b) {return a+b;}
 }
