267efb1501486079b3b29fd80b211eef0199f67a
PuPp1T <nanhu9604@gmail.com>
2022-09-21 01:31:32 +0000

    Update at 2022-09-21T01:31:32.480816Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,15 +2,15 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        hello();
-         add(40, 2);
+        System.out.println(hello());
+        System.out.println(add(40, 2));
     }
 
-    public static void hello() {
-        System.out.println("Hello");
+     public static String hello() {
+        return "Hello";
     }
 
-    public static void  add(int a,int b) {
-        System.out.println(a+b);
+    public static int add(int a,int b) {
+        return a+b;
     }
 }
