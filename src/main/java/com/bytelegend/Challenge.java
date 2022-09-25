5488ae591d8800f94a4006749ca3543c6d3f68e6
Zeon3 <1366685013@qq.com>
2022-09-25 03:11:49 +0000

    Update at 2022-09-25T03:11:49.859416Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,11 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+    public static void hello() {
+        System.out.println("hello");
+    }
 
-    public static add() {}
+    public static int add(int a, int b) {
+        return a+b;
+    }
 }
