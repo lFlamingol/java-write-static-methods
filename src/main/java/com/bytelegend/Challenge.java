5f735e2168575f27ff700e8a5a57d6259dbe1905
hhamm1314 <hhamm0624@126.com>
2022-10-09 06:33:13 +0000

    Update at 2022-10-09T06:33:13.424290Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,12 +1,37 @@
 package com.bytelegend;
 
+
+/**
+ * @author haohao
+ */
 public class Challenge {
+
+    /**
+     * 主方法
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println(hello());
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+    /**
+     * 输出 hell
+     * @return
+     */
+    public static String hello() {
+        String str = "Hello";
+        return str;
+    }
 
-    public static add() {}
+    /**
+     * 输出 add
+     * @param a
+     * @param b
+     * @return
+     */
+    public static Integer add(Integer a, Integer b) {
+        Integer inter = a + b;
+        return inter;
+    }
 }
