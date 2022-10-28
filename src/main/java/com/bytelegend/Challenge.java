c17acf2940c754a81d61b88005395212584befd3
lanzishou <524398590@qq.com>
2022-10-28 09:01:19 +0000

    Update at 2022-10-28T09:01:19.369153Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,11 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+    public static String hello() {
+        return "Hello";
+    }
 
-    public static add() {}
+    public static String add(int num,int num2) {
+        return String.valueOf(num+num2);
+    }
 }
