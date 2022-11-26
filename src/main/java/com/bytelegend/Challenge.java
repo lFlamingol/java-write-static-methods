00caf9ff6480afb6acba838a6f42550c269e746d
ApoptosisToxin4869 <jiqianlang19660417@163.com>
2022-11-26 13:19:22 +0000

    Update at 2022-11-26T13:19:22.804460Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,7 +6,11 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {}
+        public static String hello() {
+        return "Hello"; 
+    }
 
-    public static add() {}
+    public static int add(int a, int b) {
+        return a+b; 
+    }
 }
