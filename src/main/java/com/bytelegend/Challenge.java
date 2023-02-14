43b3a028b8c2337965e67b5fa85a4d60a9e36d10
qianqian1987 <18611664842@sina.cn>
2023-02-14 06:23:14 +0000

    Update at 2023-02-14T06:23:14.214732Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,12 +6,11 @@ public class Challenge {
         System.out.println(add(40, 2));
     }
 
-    public static hello() {
-        System.out.println("Hello");
+    public static String hello() {
+        return "Hello";
     }
 
-    public static add(int a, int b) {
-        int result = a+b;
-        System.out.println(result);
+    public static int  add(int a, int b) {
+        return a+b;
     }
 }
