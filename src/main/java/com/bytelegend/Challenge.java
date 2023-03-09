c96cec2e1610b4572b4017521f62e65c70584996
judemaij <judemaij@gmail.com>
2023-03-09 02:32:01 +0000

    Update at 2023-03-09T02:32:01.239725Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,17 +2,16 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        int x = add();
         hello();
-        System.out.println(x);
+        add(x);
     }
 
     public static void hello() {
         System.out.println("Hello");
     }
 
-    public static void add() {
-        int number = 4+2;
-        return number;
+    public static void add(int x) {
+        x = 40+2;
+        System.out.println(x);
     }
 }
