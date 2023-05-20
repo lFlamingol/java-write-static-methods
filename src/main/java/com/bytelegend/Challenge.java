5c4df638534378a28d227aa363da306df401d3ee
rosettastoned12 <officiallynabiel@gmail.com>
2023-05-20 16:05:53 +0000

    Update at 2023-05-20T16:05:53.884851Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,16 +1,16 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static void main(String[] args) {
-        System.out.println(hello());
-        System.out.println(add(40, 2));
-    }
 
-    public static hello() {
+    public static String hello() {
         return "Hello";
     }
 
-    public static add(int a, int b) {
+    public static int add(int a, int b) {
         return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(hello());
+        System.out.println(add(40, 2));
     }
 }
