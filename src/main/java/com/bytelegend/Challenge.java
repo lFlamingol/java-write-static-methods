1723cb11cd2d6526a427afcda16fd0b50ac1c338
周啟尧 <435184766@qq.com>
2023-06-11 23:41:38 +0800

    complete methods

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,12 +1,16 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static void main(String[] args) {
-        System.out.println(hello());
-        System.out.println(add(40, 2));
-    }
+  public static void main(String[] args) {
+    System.out.println(hello());
+    System.out.println(add(40, 2));
+  }
 
-    public static hello() {}
+  public static String hello() {
+    return "Hello";
+  }
 
-    public static add() {}
+  public static Integer add(int a, int b) {
+    return a + b;
+  }
 }
