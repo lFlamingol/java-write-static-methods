4ca758064dcc48fabfcdef69f2dae76771bd6323
cjh200300 <1053123738@qq.com>
2023-06-14 08:55:09 +0800

    new22

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,7 +10,7 @@ public class Challenge {
         return "Hello";
     }
 
-    public static int add(int number1, int number2) {
-        return number1 + number2;
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
